b861639183f44205572cebd7001d9c52a6ab4d89
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-03 20:36:35 -0700

    NUKE: restarted. wiped spec and NightWriter class

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,22 +2,7 @@
 
 class NightWriter
 
-  def initialize(filename)
-    @filename = filename
 
-  end
-
-  def read
-    file = File.open(@filename)
-    incoming = file.read
-    incoming.length
-
-  end
-
-  def welcome
-    "Created '#{@filename}' containing #{self.read} characters"
-
-  end
 
 
 end
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -6,22 +6,12 @@ SimpleCov.start
 
 RSpec.describe NightWriter do
   before(:each) do
-    @night_writer = NightWriter.new('message.txt', 'braille.txt')
+    @night_writer =
 
   end
   it 'exists' do
       expect(@night_writer).to be_a(NightWriter)
   end
-
-  it 'can read a file, give its character count' do
-    expect(@night_writer.read).to eq(27)
-  end
-
-  it 'can give a welcome message' do
-    expect(@night_writer.welcome).to eq("Created 'message.txt' containing 27 characters")
-  end
-
-
 
 
 end
