f52d6346ef799890111c1edd9bdff076804a83d7
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-04 16:17:24 -0700

    Test: add more expectations to english_to_braille test to better test the method

--- spec/alphabet_spec.rb
+++ spec/alphabet_spec.rb
@@ -14,6 +14,9 @@ RSpec.describe Alphabet do
 
   it 'has english to braille alphabet' do
     expect(@alphabet.english_to_braille).to be_a(Hash)
+    expect(@alphabet.english_to_braille.keys).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+    expect(@alphabet.english_to_braille["a"]).to eq(["O.", "..", ".."])
+      expect(@alphabet.english_to_braille["p"]).to eq(["OO", "O.", "O."])
   end
 
 
