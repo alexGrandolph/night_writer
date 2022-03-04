f2864d30b53d6cb2f91ad303d6a408e089554c4f
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-04 15:37:13 -0700

    Feat: add code for welcome method, refactor tests, mini nuke

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,47 +2,41 @@
 
 class NightWriter
 
-  def initialize(file)
-
-    @file = file
-
-
-    # @read_message = read_file
-    # @braile = create_new_file
-    # input_file = ARGV[0], output = ARGV[1]
-    # @input_file = input_file
-    # @output = output
+  def initialize(input_file = ARGV[0], output_file = ARGV[1])
+    @input_file = input_file
+    @output_file = output_file
   end
 
-  def read
-    handle = File.open(@file, 'r')
+
+  def welcome
+    handle = File.open(@input_file, 'r')
     incoming = handle.read
-    incoming
+    handle.close
+    out = incoming.upcase
+    write = File.open("output_file.txt", 'w')
+    count = write.write(out)
+    write.close
+    # require "pry"; binding.pry
+    print "Created #{ARGV[1]} containing #{count} characters"
   end
-
-  def write_new_file
-    copy_file = read.upcase
-    writer = File.open(ARGV[1], 'w')
-    new_file_count = writer.write(copy_file)
-    @new_file_count = new_file_count
-    copy_file
-  end
-
-
-
-  # def welcome
-  #   handle = File.open(@input_file)
-  #   incoming = handle.read
-  #   handle.close
-  #   out = incoming.upcase
-  #   write = File.open(@output, 'w')
-  #   n = write.write('braille.txt')
-  #   write.close
-  #   puts "Created #{@output} containing #{incoming.length} characters"
-  # end
 
 
 end
 
-# new = NightWriter.new('./spec/test/message.txt')
-# new.welcome
+new = NightWriter.new('./spec/test/message.txt')
+new.welcome
+
+
+# def read
+#   handle = File.open(@file, 'r')
+#   incoming = handle.read
+#   incoming
+# end
+#
+# def write_new_file
+#   copy_file = read.upcase
+#   writer = File.open(ARGV[1], 'w')
+#   new_file_count = writer.write(copy_file)
+#   @new_file_count = new_file_count
+#   copy_file
+# end
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -7,27 +7,28 @@ SimpleCov.start
 RSpec.describe NightWriter do
   before(:each) do
     ARGV[0] = './spec/test/message.txt'
-    ARGV[1] = './spec/test/braille.txt'
+    ARGV[1] = './spec/test/output_file.txt'
     @night_writer = NightWriter.new(ARGV[0])
   end
   it 'exists' do
-      expect(@night_writer).to be_a(NightWriter)
-  end
-
-  it 'can read given file' do
-    #
-    expect(@night_writer.read).to eq("here are a bunch of words.\n")
-  end
-
-  it 'can write to a new file' do
-    expected = @night_writer.read.upcase
-    expect(@night_writer.write_new_file).to eq(expected)
+    expect(@night_writer).to be_a(NightWriter)
   end
 
   it 'can give the welcome message' do
-    expect(@night_writer.welcome).to eq("Created #{@output} containing #{incoming.length} characters"")
+    # require "pry"; binding.pry
+    expect{@night_writer.welcome}.to output("Created ./spec/test/output_file.txt containing 27 characters").to_stdout
   end
 
 
 
 end
+
+# xit 'can read given file' do
+#   #
+#   expect(@night_writer.read).to eq("here are a bunch of words.\n")
+# end
+#
+# xit 'can write to a new file' do
+#   expected = @night_writer.read.upcase
+#   expect(@night_writer.write_new_file).to eq(expected)
+# end
