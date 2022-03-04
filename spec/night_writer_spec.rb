1e9bf64276cdd93f3131d5da1be346cd6806d8c0
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-03 17:42:54 -0700

    Test: add test for nightwriter initialization

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -5,9 +5,12 @@ SimpleCov.start
 
 
 RSpec.describe NightWriter do
-
+  before(:each) do
+    @night_writer = NightWriter.new
+    expect(@night_writer).to be_a(NightWriter)
+  end
   it 'exists' do
-    
+
   end
 
 
