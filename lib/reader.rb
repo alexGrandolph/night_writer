cb6c54e0cf1ac249e57ad9df8ba8df0479e6a3dd
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-06 15:57:31 -0700

    Feat: add get_output_file method, add to @output_file in intialize

--- lib/reader.rb
+++ lib/reader.rb
@@ -4,11 +4,15 @@ require 'translator'
 
 class Reader
 
-  attr_reader :input_file
+  attr_reader :input_file, :output_file
 
   def initialize
     @input_file = ARGV[0]
+    @output_file = get_output_file
+  end
 
+  def get_output_file
+    return ARGV[1] if ARGV[1]
   end
 
 
