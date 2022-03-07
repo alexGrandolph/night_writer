037a6661071f80c58e4cbbe1f3f5e448d72cb12a
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-07 15:22:25 -0700

    Feat: add long lines method, refactor/redo top/middle/bottom row methods to work off the return value of long_lines

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -1,4 +1,4 @@
-require 'alphabet'
+require_relative 'alphabet'
 
 
 class BrailleTranslator
@@ -22,7 +22,56 @@ class BrailleTranslator
     end
   end
 
+  def long_lines
+    top = []
+    middle = []
+    bottom = []
+    message = format_message.reject {|string| string == ""}
+    index = 0
+    counter = message.count / 3
+    counter.times do
+      top << message[index]
+      middle << message[index + 1]
+      bottom << message[index + 2]
+      index += 3
+    end
+    new_message = [top, middle, bottom]
+    new_message
+
+  end
+
   def top_row
+    top_array = long_lines[0].join.split('')
+    new_top = top_array #.join.split('')
+    final_top = []
+    new_top.each_slice(2).map do |top|
+      final_top << "#{top.first}#{top.last}"
+    end
+    final_top
+  end
+
+  def middle_row
+    mid_array = long_lines[1].join.split('')
+    new_mid = mid_array #.join.split('')
+    final_mid = []
+    new_mid.each_slice(2).map do |mid|
+      final_mid << "#{mid.first}#{mid.last}"
+    end
+    final_mid
+  end
+
+  def bottom_row
+
+    bottom_array = long_lines[2].join.split('')
+    new_bottom = bottom_array #.join.split('')
+    final_bottom = []
+    new_bottom.each_slice(2).map do |bottom|
+      final_bottom << "#{bottom.first}#{bottom.last}"
+    end
+    final_bottom
+  end
+
+  def tp
     top = []
     top << format_message[0]
     formatted_top = top.join.split('')
@@ -31,9 +80,10 @@ class BrailleTranslator
       top_array << "#{top.first}#{top.last}"
     end
     top_array
+
   end
 
-  def middle_row
+  def middl
     middle = []
     middle << format_message[1]
     formatted_mid = middle.join.split('')
@@ -45,7 +95,7 @@ class BrailleTranslator
     mid_array
   end
 
-  def bottom_row
+  def botto
     bottom = []
     bottom << format_message[2]
     formatted_bot = bottom.join.split('')
@@ -58,15 +108,15 @@ class BrailleTranslator
 
 
   def translate_message
-    whole_message = [top_row, middle_row, bottom_row].transpose
+    final_message = [top_row, middle_row, bottom_row].transpose
     all_braille = @braille.keys
-    array = []
-    whole_message.each do |collumn|
+    final_array = []
+    final_message.each do |collumn|
       if all_braille.include?(collumn)
-        array << @braille[collumn]
+        final_array << @braille[collumn]
       end
     end
-    array
+    final_array
   end
 
   def output_ready_message
@@ -75,3 +125,14 @@ class BrailleTranslator
 
 
 end
+#   whole_message = [top_row, middle_row, bottom_row] #.transpose
+#   require "pry"; binding.pry
+#   all_braille = @braille.keys
+#   array = []
+#   whole_message.each do |collumn|
+#     if all_braille.include?(collumn)
+#       array << @braille[collumn]
+#     end
+#   end
+#   array
+# end
