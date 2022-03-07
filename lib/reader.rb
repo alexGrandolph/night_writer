7e0a3bbbb308c39b0ce0053c8c4baa1b512060a6
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-06 18:39:43 -0700

    Feat: add code for read_message method

--- lib/reader.rb
+++ lib/reader.rb
@@ -15,13 +15,16 @@ class Reader
     return ARGV[1] if ARGV[1]
   end
 
+  def read_message
+    File.open(@input_file).read
+  end
+
   def welcome_message
     message = File.open(@input_file).read
     writer = File.open(@output_file, "w")
     new = writer.write(message)
     print "created #{@output_file} #{message.length} characters"
   end
-
 
 
 
---
+++ spec/braille_translator_spec.rb
@@ -0,0 +1,29 @@
+require './lib/braille_translator'
+require 'simplecov'
+require 'rspec'
+SimpleCov.start
+
+RSpec.describe BrailleTranslator do
+
+  context 'Iteration 3.1' do
+
+    before(:each) do
+      @translator = BrailleTranslator.new("O.\nOO\n..")
+    end
+
+    it 'exists' do
+      expect(@translator).to be_a(BrailleTranslator)
+    end
+
+
+
+  end
+
+
+
+
+
+
+
+
+end
