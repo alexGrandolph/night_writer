e4ecd3a5df4ea5c2c34d1ef263fd741fa0eb79a2
alex randolph <alexgeorgerandolph@gmail.com>
2022-03-07 17:59:12 -0700

    Test: add test for punctuation marks, add punctuation marks in Alphabet class

--- spec/alphabet_spec.rb
+++ spec/alphabet_spec.rb
@@ -15,10 +15,12 @@ RSpec.describe Alphabet do
 
     it 'has english to braille alphabet' do
       expect(@alphabet.english_to_braille).to be_a(Hash)
-      expect(@alphabet.english_to_braille.keys).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+      expect(@alphabet.english_to_braille.keys).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", ",", ";", ":", ".", "!", "(", ")", "?", "", "'", "-"])
       expect(@alphabet.english_to_braille["a"]).to eq(["O.", "..", ".."])
       expect(@alphabet.english_to_braille["p"]).to eq(["OO", "O.", "O."])
+      expect(@alphabet.english_to_braille["?"]).to eq(["..", "O.", "OO"])
     end
+
   end
 
   context 'Iteration 3, braille => english' do
@@ -29,14 +31,12 @@ RSpec.describe Alphabet do
 
     it 'has braille to english alphabet' do
       expect(@alphabet.braille_to_english).to be_a(Hash)
-      expect(@alphabet.braille_to_english.values).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+      expect(@alphabet.braille_to_english.values).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", ",", ";", ":", ".", "!", ")", "?", "", "'", "-"])
       expect(@alphabet.braille_to_english[["O.", "OO", "O."]]).to eq("r")
       expect(@alphabet.braille_to_english.keys.first).to be_a(Array)
     end
-
-
-
   end
+
 
 
 
